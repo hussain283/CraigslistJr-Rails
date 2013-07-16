e734cb23868d890a0cb363ade9be646553a4816b
= <=>
2013-07-15 17:44:18 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraigslistJr::Application.config.secret_token = '1cc7d74ae7e7e739c774842e306dcb181cc43aba04141106542d26670684076d91e6a60b5300d0f3a2ddfb2c441d5a4bedad2d5d3f89ccd6d6e74d6e3c8dc718'
